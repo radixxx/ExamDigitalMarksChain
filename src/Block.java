3503a42d020f1db91451f38718a3250e4e47e7c8
radixxx <radov.2011@gmail.com>
2018-05-22 18:34:51 +0300

    generate block  genesis, add block constructor

--- src/Block.java
+++ src/Block.java
@@ -4,6 +4,22 @@ import java.util.Date;
 
 public class Block {
 
+    public String hash;
+    public String previousHash;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+    public long timeStamp; //as number of milliseconds since 1/1/1970.
+    public int nonce;
+
+    //Block Constructor.
+    public Block(String previousHash ) {
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,25 @@ public class Main {
 
     public static void main (String [] Args){
 
+        //add our blocks to the blockchain ArrayList:
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); //Setup Bouncey castle as a Security Provider
 
+        //Create wallets:
+        walletA = new Wallet();
+        walletB = new Wallet();
+        Wallet coinbase = new Wallet();
+
+
+        genesisTransaction = new Transaction(coinbase.publicKey, walletA.publicKey, 100f, null);
+        genesisTransaction.generateSignature(coinbase.privateKey);	 //manually sign the genesis transaction
+        genesisTransaction.transactionId = "0"; //manually set the transaction id
+        genesisTransaction.outputs.add(new TransactionOutput(genesisTransaction.reciepient, genesisTransaction.value, genesisTransaction.transactionId)); //manually add the Transactions Output
+        UTXOs.put(genesisTransaction.outputs.get(0).id, genesisTransaction.outputs.get(0)); //its important to store our first transaction in the UTXOs list.
+
+        System.out.println("Creating and Mining Genesis block... ");
+        Block genesis = new Block("0");
+        genesis.addTransaction(genesisTransaction);
+        addBlock(genesis);
 
 
 
