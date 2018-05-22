6555ba19c25711e87ce93e3abaf73956110f1a8c
radixxx <radov.2011@gmail.com>
2018-05-22 19:05:39 +0300

    add libraris for encrypting

--- src/WalletDiary.java
+++ src/WalletDiary.java
@@ -33,7 +33,43 @@ public class WalletDiary {
         }
     }
 
-    
+
+    public float getBalance() {
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: NoobChain.UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+                UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
+                total += UTXO.value ;
+            }
+        }
+        return total;
+    }
+
+    public Transaction sendFunds(PublicKey _recipient,float value ) {
+        if(getBalance() < value) {
+            System.out.println("#Not Enough funds to send transaction. Transaction Discarded.");
+            return null;
+        }
+        ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            total += UTXO.value;
+            inputs.add(new TransactionInput(UTXO.id));
+            if(total > value) break;
+        }
+
+        Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+        newTransaction.generateSignature(privateKey);
+
+        for(TransactionInput input: inputs){
+            UTXOs.remove(input.transactionOutputId);
+        }
+
+        return newTransaction;
+    }
 
 
 }
