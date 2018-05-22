9d02588aeb7651ae1441ba51b8f6197f272fc8d1
radixxx <radov.2011@gmail.com>
2018-05-22 18:17:04 +0300

    add new class Block, and use HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>();
+
+    public static int difficulty = 3;
+    public static float minimumTransaction = 0.1f;
+    public static Wallet walletA;
+    public static Wallet walletB;
+    public static Transaction genesisTransaction;
+
     public static void main (String [] Args){
+
+
+
 
 
     }
