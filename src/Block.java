831c0a44d664701c2a9fea61741ef6349e317da5
radixxx <radov.2011@gmail.com>
2018-05-22 18:38:40 +0300

    add method mineBlock and addTransaction

--- src/Block.java
+++ src/Block.java
@@ -19,8 +19,40 @@ public class Block {
         this.hash = calculateHash(); //Making sure we do this after we set the other values.
     }
 
+    //Calculate new hash based on blocks contents
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        merkleRoot
+        );
+        return calculatedhash;
+    }
 
+    //Increases nonce value until hash target is reached.
+    public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
 
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((!"0".equals(previousHash))) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+
+        
 
 
 }
